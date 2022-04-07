1703560a25988029fc4ee50f3dd24a143cd96ab1
RlCK <kcpr.hk@gmail.com>
2022-04-08 00:07:20 +0200

    Update cam_det.py

--- src/cam_det.py
+++ src/cam_det.py
@@ -1,23 +1,40 @@
 import cv2
 import image_compare as ic
-from PIL import Image
+from PIL import Image, ImageEnhance
 import numpy as np
-w, h = 64, 64
 
-vid = cv2.VideoCapture(0)
-vid.set(3, w)
-vid.set(4, h)
-oldImg = Image.new("RGB", (w, h))
+def from_cv_to_PIL(image):
+    return Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-while (True):
 
-    ret, newFrame = vid.read()
-    cv2.imshow('frame', newFrame)
-    newImg = Image.fromarray(cv2.cvtColor(newFrame, cv2.COLOR_BGR2RGB))
+def from_PIL_to_cv(image):
+    return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-    differenceIndicator = ic.compareImages(newImg, oldImg)
-    print(differenceIndicator)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def cam_det(width = 64, heigh = 64, tolerance = 0):
 
-cv2.destroyAllWindows()
+    vid = cv2.VideoCapture(0)
+    vid.set(3, width)
+    vid.set(4, heigh)
+    oldImg = Image.new("RGB", (width, heigh))
+    oldDifferenceIndicator = 0.9
+    while (True):
+
+        ret, newFrame = vid.read()
+
+        newImg = from_cv_to_PIL(newFrame)
+        newImg = ImageEnhance.Brightness(newImg)
+        newImg.enhance(1)
+        newImg = newImg.image
+        view = from_PIL_to_cv(newImg)
+        cv2.imshow('win', view)
+
+        newDifferenceIndicator = ic.compareImages(newImg, oldImg)
+        print(abs(newDifferenceIndicator - oldDifferenceIndicator))
+        if abs(newDifferenceIndicator - oldDifferenceIndicator) < tolerance:
+            newImg.save("../database/brick.png", bitmap_format="png")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+cam_det()
