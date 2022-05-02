3662fd4853fb5236b354f7b40a8fb269726c56e7
Mateusz <nnn.2321a@gmail.com>
2022-05-02 10:54:51 +0100

    model update

--- CNN/CNN_Model.py
+++ CNN/CNN_Model.py
@@ -1,9 +1,9 @@
 from tensorflow import keras
 
 data_generator = keras.preprocessing.image.ImageDataGenerator()
-train = data_generator.flow_from_directory(directory='Data/train', class_mode='categorical', batch_size=64)
-validation = data_generator.flow_from_directory(directory='Data/validation', class_mode='categorical', batch_size=64)
-test = data_generator.flow_from_directory(directory='Data/test', class_mode='categorical', batch_size=64)
+train = data_generator.flow_from_directory(directory='Data/train', class_mode='categorical', batch_size=32)
+validation = data_generator.flow_from_directory(directory='Data/validation', class_mode='categorical', batch_size=32)
+test = data_generator.flow_from_directory(directory='Data/test', class_mode='categorical', batch_size=32)
 
 model = keras.models.Sequential()
 model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(64, 64, 3)))
@@ -11,6 +11,15 @@ model.add(keras.layers.MaxPooling2D((2, 2)))
 model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(keras.layers.MaxPooling2D((2, 2)))
 model.add(keras.layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(128, activation='relu'))
+model.add(keras.layers.Dense(200))
 model.summary()
-batchX, batchy = train.next()
-print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
+model.compile(optimizer=keras.optimizers.SGD(learning_rate=1e-3), loss=keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True), metrics=[keras.metrics.Accuracy(), keras.metrics.FalseNegatives()])
+#batchX, batchy = train.next()
+#print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
+
+history = model.fit(train, steps_per_epoch=16, validation_data=validation, validation_steps=8)
+
+loss = model.evaluate_generator(test, steps=24)
