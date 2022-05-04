1cbf9fffd62c6fdd17cd3a8b64b790d90ec5f88e
RlCK <kcpr.hk@gmail.com>
2022-05-05 01:23:45 +0200

    cc

--- CNN/prediction.py
+++ CNN/prediction.py
@@ -1,12 +1,19 @@
-import pickle
+import numpy as np
 from tensorflow import keras
 
 
-def make_pred(image):
-    image = keras.preprocessing.image.load_img(image)
-    loaded_model = pickle.load(open("CNN_Model_final.sav", 'rb'))
-    prediction = loaded_model.predict_classes(image)
-    print(prediction)
-    return prediction
 
-make_pred("brick.png")
+
+def make_pred(image):
+
+    image = keras.preprocessing.image.load_img(image, target_size=(64, 64))
+    image_arr = keras.preprocessing.image.img_to_array(image)
+    image_batch = np.expand_dims(image_arr, axis=0)
+    image_preproc = keras.applications.resnet50.preprocess_input(image_batch)
+    model = keras.models.load_model("D:\\Lego-sorter\\CNN\\CNN_Model_final.h5")
+
+    prediction = model.predict(image_preproc)
+    classes = np.argmax(prediction, axis=1)
+    print(classes)
+
+make_pred("D:\\Lego-sorter\\CNN\\brick.png")
