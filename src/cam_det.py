e685733a16bdf7c3a0d218d038526b15f04ba2fe
RlCK <kcpr.hk@gmail.com>
2022-05-11 00:41:25 +0200

    sda

--- src/cam_det.py
+++ src/cam_det.py
@@ -22,21 +22,10 @@ def image_sharpening(image):
 
 def image_contrasting(img):
     lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    cv2.imshow("lab", lab)
-
-    # -----Splitting the LAB image to different channels-------------------------
     l, a, b = cv2.split(lab)
-
-    # -----Applying CLAHE to L-channel-------------------------------------------
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     cl = clahe.apply(l)
-
-
-    # -----Merge the CLAHE enhanced L-channel with the a and b channel-----------
     limg = cv2.merge((cl, a, b))
-
-
-    # -----Converting image from LAB Color model to RGB model--------------------
     final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
 
     return final
